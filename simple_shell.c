9b3223698cfc334ceb545b7f612da6413ed8c39e
alemao51092 <aarielmartinez1111@gmail.com>
2023-04-21 12:34:45 -0700

    simpleshell

---
+++ simple_shell.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+int main(void)
+{
+    char *comand = NULL;
+	size_t n = 0;
+	char **tokenargv;
+    int read = 0;
+
+    while(1)
+    {
+        if (isatty(0) == 1)
+            printf("Ale ~$: ");
+            read = getline(&comand,&n, stdin);
+		if(read == -1)
+		{
+		    printf("\n");
+		    exit (1);
+		}
+    }
+}
